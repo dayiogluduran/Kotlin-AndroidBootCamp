2720d66be52b73df7d5034bd4773023d3fbae312
Duran DAYIOGLU <dayiogluduran@gmail.com>
2019-02-10 14:14:43 +0300

    Initial commit

---
+++ src/VizeFinal.kt
@@ -0,0 +1,32 @@
+//┌─────────────────────────────┐
+//│ Created by Duran DAYIOĞLU   │
+//│ ─────────────────────────── │
+//│ dayiogluduran@gmail.com     │
+//│ ─────────────────────────── │
+//│ 2019-02-10 - 13:14          │
+//└─────────────────────────────┘
+
+// TODO(1) Kullanıcıdan adını alın.
+// TODO(2) Kullanıcıdan soyadını alın.
+// TODO(3) Kullanıcıdan vize notunu alın.
+// TODO(4) Kullanıcıdan final notunu alın.
+// TODO(5) Vize'nin %40'ını Final'in %60'ını alan ve geciş notunu print eden bir infix-extention method yazınız.
+fun main() {
+    println("Adınızı Giriniz: ")
+    val name = readLine()
+
+    println("Soyadınızı Giriniz: ")
+    val surName = readLine()
+
+    println("Vize Notunuzu Giriniz: ")
+    val vizeNotu = readLine()
+
+    println("Final Notunuzu Giriniz: ")
+    val finalNotu = readLine()
+
+    print("$name $surName ortalamanız: ")
+    println(vizeNotu!!.toInt() ortalama (finalNotu!!.toInt()))
+
+}
+
+infix fun Int.ortalama(not: Int): Int = ((this * 40 / 100) + (not * 60 / 100))
